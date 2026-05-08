e5f7cb562252212c7ed1d85c6f851752a35d228d
gganghw <sunflower110714@gmail.com>
2026-05-08 03:07:39 +0000

    to 3-1

---
+++ code/3-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct Node *head = &p;
+
+
+add<name><score>
+
+delete<name>
+
+void print(){
+for(struct Node *p = head; p !=null; p = p->next){
+    printf("%s %d", name, score)
+}
+}
